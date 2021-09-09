ffad4c5e538a609930198309c54a7665248f915d
rakshith <rakshit.s.gowda@gmail.com>
2021-09-09 09:08:51 +0530

    header toolbar links

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Container, Link, Toolbar, Typography } from '@material-ui/core'
 import Head from 'next/head'
 import useStyles from '../utils/styles'
+import NextLink from 'next/link'
 
 export default function Layout({ children }) {
   const classes = useStyles()
@@ -11,7 +12,20 @@ export default function Layout({ children }) {
       </Head>
       <AppBar position='static' className={classes.navbar}>
         <Toolbar>
-          <Typography>Amazonclone</Typography>
+          <NextLink href='/' passHref>
+            <Link>
+              <Typography className={classes.brand}>Amazonclone</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href='/cart' passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href='/login' passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
